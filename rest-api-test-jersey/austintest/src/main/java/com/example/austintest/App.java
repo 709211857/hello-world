295bd97084bc4a8283e24b7c645d597c505397d9
709211857 <709211857@qq.com>
2017-06-20 11:04:09 +0800

    version initialization

---
+++ rest-api-test-jersey/austintest/src/main/java/com/example/austintest/App.java
@@ -0,0 +1,13 @@
+package com.example.austintest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
